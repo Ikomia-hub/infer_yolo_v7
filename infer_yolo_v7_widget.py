11cd8d590f6ccc3af2c4a3515cd244c98ce2626c
Allan <allan.kouidri@gmail.com>
2023-02-16 11:02:26 +0100

    Add param

--- infer_yolo_v7_widget.py
+++ infer_yolo_v7_widget.py
@@ -43,48 +43,39 @@ class InferYoloV7Widget(core.CWorkflowTaskWidget):
         # Create layout : QGridLayout by default
         self.gridLayout = QGridLayout()
 
-        self.check_cuda = pyqtutils.append_check(self.gridLayout, "Cuda", self.parameters.cuda and is_available())
-        self.check_cuda.setEnabled(is_available())
-        self.spin_img_size = pyqtutils.append_spin(self.gridLayout, "Image size", self.parameters.img_size)
+        # Image size
+        self.spin_imgsz = pyqtutils.append_double_spin(self.gridLayout, "Input size",
+                                                          self.parameters.imgsz,
+                                                          min=0., max=2000, step=32, decimals=2)
+        # Confidence threshold
         self.spin_thr_conf = pyqtutils.append_double_spin(self.gridLayout, "Confidence threshold",
                                                           self.parameters.thr_conf,
                                                           min=0., max=1., step=0.01, decimals=2)
-        self.spin_iou_conf = pyqtutils.append_double_spin(self.gridLayout, "Confidence IOU", self.parameters.iou_conf,
+
+        # Confidence IOU
+        self.spin_iou_conf = pyqtutils.append_double_spin(self.gridLayout, "Confidence IOU",
+                                                          self.parameters.iou_conf,
                                                           min=0., max=1., step=0.01, decimals=2)
-        self.check_half_precision = pyqtutils.append_check(self.gridLayout, "Half precision", self.parameters.fp16)
-        self.check_half_precision.setEnabled(True)
-        self.check_custom_train = pyqtutils.append_check(self.gridLayout, "Custom train", self.parameters.custom_train)
-        self.check_custom_train.stateChanged.connect(self.on_custom_train_changed)
-        self.combo_pretrain_model = pyqtutils.append_combo(self.gridLayout, "Model name")
-        for name in model_zoo.keys():
-            self.combo_pretrain_model.addItem(name)
-        self.combo_pretrain_model.setCurrentText(self.parameters.pretrain_model)
-        self.combo_pretrain_model.setEnabled(not self.parameters.custom_train)
-        self.browse_custom_model = pyqtutils.append_browse_file(self.gridLayout, "Custom model",
-                                                                self.parameters.custom_model)
-        self.browse_custom_model.setEnabled(self.parameters.custom_train)
+        # Model path (.onnx)
+        self.browse_model = pyqtutils.append_browse_file(self.gridLayout,
+                                                label="Model path (.onnx)",
+                                                path=self.parameters.weights,
+                                                mode=QFileDialog.ExistingFile)
+
         # PyQt -> Qt wrapping
         layout_ptr = qtconversion.PyQtToQt(self.gridLayout)
 
         # Set widget layout
         self.setLayout(layout_ptr)
 
-    def on_custom_train_changed(self, name):
-        self.combo_pretrain_model.setEnabled(not self.check_custom_train.isChecked())
-        self.browse_custom_model.setEnabled(self.check_custom_train.isChecked())
 
     def onApply(self):
         # Apply button clicked slot
         # Get parameters from widget
-        # Example : self.parameters.windowSize = self.spinWindowSize.value()
-        self.parameters.cuda = self.check_cuda.isChecked()
-        self.parameters.fp16 = self.check_half_precision.isChecked()
-        self.parameters.custom_train = self.check_custom_train.isChecked()
-        self.parameters.img_size = self.spin_img_size.value()
-        self.parameters.pretrain_model = self.combo_pretrain_model.currentText()
+        self.parameters.weights = self.browse_model.path
+        self.parameters.imgsz = self.spin_imgsz.value()
         self.parameters.iou_conf = self.spin_iou_conf.value()
         self.parameters.thr_conf = self.spin_thr_conf.value()
-        self.parameters.custom_model = self.browse_custom_model.path
         self.parameters.update = True
         # Send signal to launch the process
         self.emitApply(self.parameters)
